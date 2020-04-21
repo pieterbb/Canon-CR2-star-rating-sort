0a8460accff8a209b48bce5fda6c7062f8b62e5b
Pieter <hello@piet.re>
2020-04-22 00:02:28 +0200

    Refactored code, removed functions and fixed the move_to_folders functions

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# Python 2.7 for compatibility with EXIFRead. Sigh...
-# ctrl - alt - n for running
-# Todo: Remove output folder, move photo's instead from the folder its in.
-# Todo: Port to virtualenv. to keep dependencies consistent
-# Todo: Doublecheck the files is closed in the read method so it doesn't hog up memory
-
-
 import exifread  # External Library
 import os
 import shutil
 import re
-import time
 import glob
 import collections  # for ordered dictionary
-import datetime
 from itertools import groupby
 from bs4 import BeautifulSoup
 
@@ -27,22 +18,22 @@ def return_star_rating(filepath):
     if xmp_start != xmp_end:
         xmpString = imgAsString[xmp_start:xmp_end+12]
 
-    xmpAsXML = BeautifulSoup(xmpString, features="html")
+    xmpAsXML = BeautifulSoup(xmpString, features="lxml")
     star_rating = int(xmpAsXML.find('xmp:rating').text)
 
     return (star_rating)
 
 
-def move_photos_to_folders(cr2_category):
+def move_photos_to_folders(star_rating):
     # iterate over the photos and create the right file path
-    for item, key in cr2_category.iteritems():
+    for item, key in star_rating.iteritems():
         if not os.path.exists(input_folder + "/" + str(key)):
             os.makedirs(input_folder + "/" + str(key))
 
         # Move the photos
         file_name = re.search("\/([^\/]+)$", item)
-        shutil.move(input_folder + "/" + file_name.group(1), output_folder + "/" +
-                    str(key) + "/" + file_name.group(1))
+        shutil.move(input_folder + "/" + file_name.group(1),
+                    input_folder + "/" + str(key) + "/" + file_name.group(1))
 
 
 def folder_to_filelist(folder_path):
@@ -52,8 +43,6 @@ def folder_to_filelist(folder_path):
     # Only add CR2 photo files to the list
     for name in sorted(glob.iglob(folder_path + '/*.CR2')):
         file_list.append(name)
-        print(name)
-    # Sort the file list by number
     return file_list
 
 
@@ -61,47 +50,26 @@ def process_folder(folder):
     # Get a list with all the CR2 files in the folder we are processing
     file_list = folder_to_filelist(folder)
 
-    # Extract the photorating out of the CR2 file into a sorted dictionary
+    # Datatype that remembers the order entries where added. Key:Value
     photo_rating = collections.OrderedDict()
 
+    # Extract the photorating out of the CR2 file into a sorted dictionary for all files
     for file in file_list:
         photo_rating[file] = return_star_rating(file)
-        print str(file) + " - Photo Rating: " + \
-            str(return_star_rating(file))
+        print str(file) + " -  Rating: " + \
+            str(photo_rating[file])
+        os.system('clear')
 
-    photoRatingDict = collections.OrderedDict()
-
-    move_photos_to_folders(photoRatingDict)
+    move_photos_to_folders(photo_rating)
 
 
 def main():
-    print "____________ \n \n Canon Star Sorter V.01"
-
+    print "____________ \n \n RatingSort: Sort photo's by Star Rating and move to folder V.01"
     global input_folder
-    global output_folder
 
-    #input_folder = raw_input("\n Drag in your input folder and press enter: ")
+    # input_folder = raw_input("\n Drag in your input folder and press enter to start: ")
     input_folder = "/Users/Kevin/Downloads/Jack Script/Day 2 copsdfy/0/"
-
-    # output_folder = input("\n Drag in your output folder and press enter: ")
-    #input_folder = input_folder.replace("\\", "") + "/"
-    print input_folder
-    output_folder = input_folder
-
-    # Replace with function to scan for folders and generate list of folders to process.
-    # Double check the syntax here with dashes at the end, see how the file path comes out of the folder scan lanter
-
-    folder_list = [input_folder]
-    # return folder list, return only folders with footage inside them. Otherwise traverse deeper.
-    # only folders to be actually processed.
-
-    # Scan folder for CR2 files and assign them a timelapse number.
-    for folder in folder_list:
-        # This returns a dictionary of files
-        process_folder(folder)
-
-        # Print out the results and sleep. later add a function to move them.  Set color labels?
+    process_folder(input_folder)
 
 
-# Run Main thread
 main()
