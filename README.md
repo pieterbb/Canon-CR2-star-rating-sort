693f2e5419c81646f087535653432227bce43e4b
Pieter <pieter@pieters-mbp.home>
2020-04-28 21:19:30 +0200

    Refactored code, added working file move module

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Canon-CR2-Offload-by-star-rating
-Sort Canon CR2 Raw Files in folders per star rating
+A tool for Canon photographers who rate CR2 photo's on their camera.
+
+This tool sorts CR2 photos in rated and unrated folders. This way you can chose to only import the rated CR2 photo's into lightroom for example.
--- main.py
+++ main.py
@@ -1,76 +1,74 @@
-import exifread  # External Library
 import os
 import shutil
 import re
 import glob
-import collections  # for ordered dictionary
-from itertools import groupby
-from bs4 import BeautifulSoup
+# from itertools import groupby
 
+#TODO: Make folder scanning recursive
+#TODO: Package as EXE for Windows
 
 def return_star_rating(filepath):
     # Extract the XMP data from the Canon RAW CR2 file and return the star rating.
     # Canon Stores star rating in additional XMP not EXIF
     with open(filepath, "rb") as file:
-        img = file.read()
-    imgAsString = str(img)
-    xmp_start = imgAsString.find('<x:xmpmeta')
-    xmp_end = imgAsString.find('</x:xmpmeta')
-    if xmp_start != xmp_end:
-        xmpString = imgAsString[xmp_start:xmp_end+12]
+        for line in file.readlines():
+            if re.search("<x:xmpmeta",  str(line), 0):
+                star_rating_match = re.findall("<xmp:Rating>(\d)<\/xmp:Rating>", str(line))
 
-    xmpAsXML = BeautifulSoup(xmpString, features="lxml")
-    star_rating = int(xmpAsXML.find('xmp:rating').text)
-
-    return (star_rating)
-
-
-def move_photos_to_folders(star_rating):
-    # iterate over the photos and create the right file path
-    for item, key in star_rating.iteritems():
-        if not os.path.exists(input_folder + "/" + str(key)):
-            os.makedirs(input_folder + "/" + str(key))
-
-        # Move the photos
-        file_name = re.search("\/([^\/]+)$", item)
-        shutil.move(input_folder + "/" + file_name.group(1),
-                    input_folder + "/" + str(key) + "/" + file_name.group(1))
-
-
-def folder_to_filelist(folder_path):
-    print "Scanning: " + str(folder_path)
-    # Scan folder for CR2 Photos files and return full file filepath.
-    file_list = []
-    # Only add CR2 photo files to the list
-    for name in sorted(glob.iglob(folder_path + '/*.CR2')):
-        file_list.append(name)
-    return file_list
+                # Catch NoneTypes, make sure you always return an int
+                if star_rating_match:
+                    star_rating = int(star_rating_match[0])
+                else:
+                    star_rating = 0
+                return (star_rating)
 
 
 def process_folder(folder):
-    # Get a list with all the CR2 files in the folder we are processing
-    file_list = folder_to_filelist(folder)
+    file_list = []
 
-    # Datatype that remembers the order entries where added. Key:Value
-    photo_rating = collections.OrderedDict()
+    # Scan folder for CR2 Photos files and return full filepaths
+    # Only add CR2 photo files to the list
+    for name in sorted(glob.iglob(folder + '/*.CR2')):
+        file_list.append(name)
 
-    # Extract the photorating out of the CR2 file into a sorted dictionary for all files
+    print (str(len(file_list)) + " CR2 files found in input folder...")
+
+    # Loop over the photo files and check their ratings
     for file in file_list:
-        photo_rating[file] = return_star_rating(file)
-        print str(file) + " -  Rating: " + \
-            str(photo_rating[file])
-        os.system('clear')
+        photo_rating = return_star_rating(file)
+        print (str(file) + " -  Rating: " + \
+            str(photo_rating))
+        
+        # Check if photo has a rating 
+        if photo_rating is 0:
+            # Check if unrated folder already exists otherwise create it
+            if not os.path.exists(input_folder + "/unrated"):
+                os.makedirs(input_folder + "/unrated")
 
-    move_photos_to_folders(photo_rating)
+            #Get the filename from the filepath
+            file_name = re.search("\/([^\/]+)$", file)
+            shutil.move(file, input_folder + "/unrated/" + file_name[0])
+                        
+        # If the photo has a rating above 1 
+        elif photo_rating >= 1: 
 
+            # check if rated folder already exists, otherwise create it
+            if not os.path.exists(input_folder + "/rated"):
+                os.makedirs(input_folder + "/rated")
+    
+            # Move the photo file to the folder
+            file_name = re.search("\/([^\/]+)$", file)
+            shutil.move(file, input_folder + "/rated/" + file_name[0])
 
 def main():
-    print "____________ \n \n RatingSort: Sort photo's by Star Rating and move to folder V.01"
+
+    print("""\
+Canon CR2 RatingSort: Sort photo's in folder by rated and unrated V.01
+
+    """)
     global input_folder
-
-    # input_folder = raw_input("\n Drag in your input folder and press enter to start: ")
-    input_folder = "/Users/Kevin/Downloads/Jack Script/Day 2 copsdfy/0/"
+    input_folder = "/Users/pieter/Downloads/input"
+    #input_folder = input("Drag in your input folder and press enter: ")
     process_folder(input_folder)
-
 
 main()
